d6c0b13ea9c2cde64942c9538d6c54630b6e9be1
Ewicks <ethanowicks9@gmail.com>
2022-05-18 16:07:59 +0000

    Add test code to mute audio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,17 @@
-// let incorrectSound;
-// // let isPlaying = true;
-// let muteBtn = document.getElementById('un-muted');
+let incorrectSound;
+let muteBtn = document.getElementById('un-muted');
 
 
-// muteBtn.addEventListener('click', function(){
-//     if (isPlaying) {
-//         // pause the audio track
-//         muteBtn.src = "assets/images/muted.jpeg";
-//         isPlaying = false;
-//         // sessionStorage.setItem("isPlaying", isPlaying);
-//     } else {
-//         // play the track
-//         muteBtn.src = "assets/images/speaker.png";
-//         isPlaying = true;
-//         // sessionStorage.setItem("isPlaying", isPlaying);
-//     }
-// })
+muteBtn.addEventListener('click', function(){
+    if (isPlaying) {
+        // pause the audio track
+        muteBtn.src = "assets/images/muted.jpeg";
+        isPlaying = false;
+        // correctSound.muted = true; //mute audio
+        // incorrectSound.muted = true;
+    } else {
+        // play the track
+        muteBtn.src = "assets/images/speaker.png";
+        isPlaying = true;
+    }
+})
