dfe9d35e4f318ea1aee7cd36f0b76c3612ac6bc0
Ewicks <ethanowicks9@gmail.com>
2022-05-19 17:09:42 +0000

    Add a comment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 11 */
+
 document.addEventListener("DOMContentLoaded", function () {
     // on page load, hide the preloader animation
     document.getElementById("preloader").style.opacity = 0;
